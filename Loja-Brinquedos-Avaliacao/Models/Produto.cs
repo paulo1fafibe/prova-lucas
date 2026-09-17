fe98966df27276795f81839f27d988ed03511f67
paulo1fafibe <paulo.santos08@aluno.unifafibe.edu.br>
2026-09-17 20:43:52 -0300

    Add files via upload

---
+++ Loja-Brinquedos-Avaliacao/Models/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja_Brinquedos_Avaliacao.Models
+{
+    public class Produto : Fabricante
+    {
+        public string CodigoBarras { get; set; }
+        public string Descricao { get; set; }
+        public Double Preco { get; set; }
+
+        public string CodigoBarraDescricao
+        {
+            get
+            {
+                return CodigoBarras + " - " + Descricao;
+            }
+        }
+
+        public class ProdutoLista : List<Produto>
+        {
+
+        }
+    }
+}
