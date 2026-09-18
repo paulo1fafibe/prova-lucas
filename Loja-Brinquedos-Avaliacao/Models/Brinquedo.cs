181481093ece62b8a87362c7502e227f4a242af5
paulo1fafibe <paulo.santos08@aluno.unifafibe.edu.br>
2026-09-17 21:04:35 -0300

    Descoberta
    para aparecer a alteração no GitHubDesktop agr vc precisa salvar (ctrl + S) e assim podera publicar no repo

--- Loja-Brinquedos-Avaliacao/Models/Brinquedo.cs
+++ Loja-Brinquedos-Avaliacao/Models/Brinquedo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//boa neguin
 namespace Loja_Brinquedos_Avaliacao.Models
 {
     public class Brinquedo : Produto
